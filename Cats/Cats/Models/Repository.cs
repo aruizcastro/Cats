ab49d363716cc4cd6c70dc25bc5edf8ba10d1b12
Aland <ar1987@hotmail.com>
2017-03-28 07:19:51 -0500

    subida del proyecto

---
+++ Cats/Cats/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models
+{
+    class Repository
+    {
+
+        public async Task<List<Cat>> GetCats()
+        {
+            var Service = new Services.AzureService<Cat>();
+            var Items = await Service.GetTable();
+            return Items.ToList();
+        }
+
+    }
+
+
+
+
+}
